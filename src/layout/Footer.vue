19287b060a27de14376b93cc16374ca0989d20a4
liuhj23 <liuhj23@asiainfo.com>
2026-03-15 15:44:04 +0800

    [feat] init project

---
+++ src/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+    物流管理系统 ©Created by 刘浩杰
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
